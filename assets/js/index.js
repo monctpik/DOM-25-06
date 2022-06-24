fc2688a56fcc6125245c159b988e45cc56078d37
Vladislav Moruga <Monctpik123@gmail.com>
2022-06-24 16:05:45 +0300

    Example calls

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,25 @@
-'use strict';
+"use strict";
 
-function btnHandler(){
-  console.log('button is clicked')
-}
-//const [btn] = document.getElementsByTagName('button');
-//console.log(btn)
-//const btn = document.getElementById('btn');
-const btn = document.querySelector('.btn+.btn');
+// function btnHandler(){
+//   console.log('button is clicked')
+// }
+// //const [btn] = document.getElementsByTagName('button');
+// //console.log(btn)
+// //const btn = document.getElementById('btn');
+// const btn = document.querySelector('.btn+.btn');
 
-const btns1 = document.getElementsByClassName('btn');
-const btns2 = document.querySelectorAll('.btn');
+// const btns1 = document.getElementsByClassName('btn');
+// const btns2 = document.querySelectorAll('.btn');
 
-//btn.addEventListener('click', btnHandler);
+// //btn.addEventListener('click', btnHandler);
+
+const ul = document.getElementsByTagName('ul')[0]
+const li = document.querySelector('li:nth-child(4)')
+const h2 = document.getElementsByTagName('h2')
+const img = document.querySelector('img[alt=sea]')
+const p = document.querySelector('img[alt=sea]+.text')
+console.log(ul);
+console.log(li);
+console.log(h2);
+console.log(img);
+console.log(p);
