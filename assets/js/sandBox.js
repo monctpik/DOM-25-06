b42cdfb9d0161ae3e7d1ad717b3ac8cc126f339d
Vladislav Moruga <Monctpik123@gmail.com>
2022-06-27 15:48:23 +0300

    add counter v2.0

--- assets/js/sandBox.js
+++ assets/js/sandBox.js
@@ -3,9 +3,19 @@
 
 function createCounter(initial) {
   let i = initial;
-  return function counter() {
-    i++;
-    return i;
-  };
+  return{
+    increment(){
+        return ++i;
+    },
+    decrement(){
+        return --i;
+    }
+  }
 }
-const counter1 = createCounter()
+
+const counter1 = createCounter(10);
+counter1.increment()
+counter1.increment()
+console.log(counter1.increment())
+
+const counter2 = createCounter(100);
